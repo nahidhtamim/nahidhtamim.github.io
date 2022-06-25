f09443a354894fb82f28c1b7f46cef68ef1429f0
N H Tamim <nahidhtamim@gmail.com>
2022-06-25 18:05:51 +0600

    update

--- js/script.js
+++ js/script.js
@@ -20,3 +20,68 @@ function hide() {
         }
     }
 }
+
+(function() {
+    "use strict";
+
+    var $projects = $(".projects");
+
+    $projects.isotope({
+        itemSelector: ".item",
+        layoutMode: "fitRows"
+    });
+
+    $("ul.filters > li").on("click", function(e) {
+        e.preventDefault();
+
+        var filter = $(this).attr("data-bs-filter");
+
+        $("ul.filters > li").removeClass("active");
+        $(this).addClass("active");
+
+        $projects.isotope({
+            filter: filter
+        });
+    });
+
+    $(".project")
+        .mouseenter(function() {
+            $(this)
+                .find(".project-overlay")
+                .css({
+                    top: "-100%"
+                });
+            $(this)
+                .find(".project-hover")
+                .css({
+                    top: "0"
+                });
+        })
+        .mouseleave(function() {
+            $(this)
+                .find(".project-overlay")
+                .css({
+                    top: "0"
+                });
+            $(this)
+                .find(".project-hover")
+                .css({
+                    top: "100%"
+                });
+        });
+})(jQuery);
+
+(function() {
+    var words = [
+            'Web Developer',
+            'Web Designer',
+            'Freelancer',
+        ],
+        i = 0;
+    setInterval(function() {
+        $('#profession').fadeOut(function() {
+            $(this).html(words[i = (i + 1) % words.length]).fadeIn();
+        });
+    }, 2000);
+
+})();
